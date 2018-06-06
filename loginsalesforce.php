b720a96d258479542a7ae30403be392967c48013
goginenivamsisujan <goginenivamsisujan@gmail.com>
2018-06-06 20:14:58 +0530

    Update loginsalesforce.php

--- loginsalesforce.php
+++ loginsalesforce.php
@@ -26,6 +26,5 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-  header('Content-Type: application/json');
-  echo json_encode($response);
+  echo $response.access_token;
 }
